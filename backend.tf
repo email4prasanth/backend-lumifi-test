62b230ba9f4469c395771ca4be3464b34cba71eb
email4prasanth <email4prasanth@gmail.com>
2025-06-15 07:01:22 +0530

    adding be resources files

---
+++ backend.tf
@@ -0,0 +1,9 @@
+# Terraform Remote Backend Configuration - S3 for backend code
+terraform {
+  backend "s3" {
+    bucket  = "lumifitfstore"
+    key     = "backend/terraform.tfstate"
+    region  = "us-east-1"
+    # profile = "lumifitest"
+  }
+}
