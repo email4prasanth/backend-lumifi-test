bc2bed682dd7ee2f8416d5689f8ce8931f5a51c4
email4prasanth <email4prasanth@gmail.com>
2025-07-11 15:16:00 +0530

    testing backend

--- backend-infra/serverlessLambda.tf
+++ backend-infra/serverlessLambda.tf
@@ -1,11 +1,11 @@
-data "aws_lambda_function" "serverless_lambda" {
-  function_name = "${terraform.workspace}-serverless-lambda" # Match your serverless deploy name
-}
+# data "aws_lambda_function" "serverless_lambda" {
+#   function_name = "${terraform.workspace}-serverless-lambda" # Match your serverless deploy name
+# }
 
-resource "aws_lambda_permission" "serverless_apigw" {
-  statement_id  = "AllowServerlessAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = data.aws_lambda_function.serverless_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.serverless_api.execution_arn}/*/*"
-}
+# resource "aws_lambda_permission" "serverless_apigw" {
+#   statement_id  = "AllowServerlessAPIGatewayInvoke"
+#   action        = "lambda:InvokeFunction"
+#   function_name = data.aws_lambda_function.serverless_lambda.function_name
+#   principal     = "apigateway.amazonaws.com"
+#   source_arn    = "${aws_apigatewayv2_api.serverless_api.execution_arn}/*/*"
+# }
