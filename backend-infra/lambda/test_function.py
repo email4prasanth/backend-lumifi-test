4e148ba730f28350dc8e1455f5cedfbb366aaf9a
email4prasanth <email4prasanth@gmail.com>
2025-07-13 22:14:12 +0530

    testing s3, rds, secrets

--- backend-infra/lambda/test_function.py
+++ backend-infra/lambda/test_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Lumifi test form Lambda second time!')
+        'body': json.dumps('Lumifi test form Lambda third time!')
     }
