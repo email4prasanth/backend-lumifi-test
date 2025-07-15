8b041737ba1216ed3833d3d95699a29d12f4d061
email4prasanth <email4prasanth@gmail.com>
2025-07-15 08:17:59 +0530

    deploy sls

--- backend-infra/sls_lambda.tf
+++ backend-infra/sls_lambda.tf
@@ -1,8 +1,8 @@
-# # IAM policy for API Gateway to invoke Lambda
-# resource "aws_lambda_permission" "apigw" {
-#   statement_id  = "AllowAPIGatewayInvoke"
-#   action        = "lambda:InvokeFunction"
-#   function_name = "${terraform.workspace}-lumifi-api"
-#   principal     = "apigateway.amazonaws.com"
-#   source_arn    = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
-# }
+# IAM policy for API Gateway to invoke Lambda
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${terraform.workspace}-lumifi-api"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
+}
