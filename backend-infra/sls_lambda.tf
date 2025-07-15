fec625bc1938b3f29eca79dba676871e49d5ff3a
email4prasanth <email4prasanth@gmail.com>
2025-07-15 08:55:45 +0530

    commented api gateway

--- backend-infra/sls_lambda.tf
+++ backend-infra/sls_lambda.tf
@@ -1,8 +1,9 @@
-# # IAM policy for API Gateway to invoke Lambda
-# resource "aws_lambda_permission" "apigw" {
-#   statement_id  = "AllowAPIGatewayInvoke"
-#   action        = "lambda:InvokeFunction"
+# IAM policy for API Gateway to invoke Lambda
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
 #   function_name = "${terraform.workspace}-lumifi-api"
-#   principal     = "apigateway.amazonaws.com"
-#   source_arn    = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
-# }
+  function_name = "serverless-express-app"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
+}
