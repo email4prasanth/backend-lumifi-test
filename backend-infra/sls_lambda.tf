019ef1da63d25666c514b8763de8c5cb70d895bc
email4prasanth <email4prasanth@gmail.com>
2025-07-15 10:30:27 +0530

    commented api gateway

--- backend-infra/sls_lambda.tf
+++ backend-infra/sls_lambda.tf
@@ -2,7 +2,8 @@
 resource "aws_lambda_permission" "apigw" {
   for_each = local.lambda_functions
 
-  statement_id  = "AllowAPIGatewayInvoke-${each.key}"
+#   statement_id  = "AllowAPIGatewayInvoke-${each.key}"
+  statement_id  = "AllowAPIGatewayInvoke-${terraform.workspace}-${each.key}"
   action        = "lambda:InvokeFunction"
   function_name = "serverless-express-app-${terraform.workspace}-${each.value}"
   principal     = "apigateway.amazonaws.com"
